b974172cbb449710ac9d94ffadd4bd4f14b5e0d2
charper3602 <121520481+charper3602@users.noreply.github.com>
2023-06-05 01:45:07 -0500

    Embedded.js

--- Embedded.js
+++ Embedded.js
@@ -6,7 +6,6 @@ function encoden(str){
     var info= document.getElementById("Information").value;
     var inter=encoden(info);
     var inter3=[];
-    var geno2= new Converter();
     for(let i=0; i<(inter.length);i++){
        inter3.push(getCharCodes(inter[i]));
     }
@@ -16,22 +15,6 @@ function encoden(str){
      }
      inter2.unshift(inter[0]);
      QR_gen(inter2);
-     return inter2; 
-  }
-  function p_seqence_only(){
-    var info= document.getElementById("Information").value;
-    var inter=encoden(info);
-    var inter3=[];
-    for(let i=0; i<(inter.length);i++){
-       inter3.push(getCharCodes(inter[i]));
-    }
-    var inter2=[];
-    for(let i=0; i<(inter.length)-1;i++){
-        inter2.push(inter3[i]-inter3[i+1]);
-     }
-     inter2.push((inter2.length)+1);
-     inter2.unshift(inter[0]);
-     alert("p seqence:" + inter2);
      return inter2; 
   }
   function p_display(){
@@ -44,7 +27,6 @@ function encoden(str){
         var inter= encoden(tot);
     document.getElementById("demo").innerHTML= inter[0];
     if(inter.length>1){
-     var geno2= new Converter();
     var letter= getCharCodes(inter[0]);
     var dif= letter-BigInt(inter[1]);
     var convert=dif
@@ -58,7 +40,7 @@ function encoden(str){
     inter2.splice(1,1);
     QR_gen(inter2);}
    else{
-        document.getElementById('qrcode').style.display = 'none';
+        document.getElementById('qrcode2').style.display = 'none';
     }
         html5QrcodeScanner.clear();
 }
@@ -85,60 +67,4 @@ return BigInt(charCodeArr.join(''));
           }
            var P3 = p3.toString();
            makeQR(P3)
-        }
-        function formatBigInt(values, base) {
-            //convert array of digit values to bigint string
-            for (var bigint = '', i = 0; i < values.length; i++) {
-              bigint += values[i].toString(base);
-            }
-            return bigint;
-          }      
-        function parseBigInt(bigint, base) {
-            //convert bigint string to array of digit values
-            for (var values = [], i = 0; i < bigint.length; i++) {
-              values[i] = parseInt(bigint.charAt(i), base);
-            }
-            return values;
-          }
-        function convertBase(bigint, inputBase, outputBase) {
-            //takes a bigint string and converts to different base
-            var inputValues = parseBigInt(bigint, inputBase),
-              outputValues = [], //output array, little-endian/lsd order
-              remainder,
-              len = inputValues.length,
-              pos = 0,
-              i;
-            while (pos < len) { //while digits left in input array
-              remainder = 0; //set remainder to 0
-              for (i = pos; i < len; i++) {
-                //long integer division of input values divided by output base
-                //remainder is added to output array
-                remainder = inputValues[i] + remainder * inputBase;
-                inputValues[i] = Math.floor(remainder / outputBase);
-                remainder -= inputValues[i] * outputBase;
-                if (inputValues[i] == 0 && i == pos) {
-                  pos++;
-                }
-              }
-              outputValues.push(remainder);
-            }
-            outputValues.reverse(); //transform to big-endian/msd order
-            return formatBigInt(outputValues, outputBase);
-          }  
-          class Converter{
-            toASCII = (hex = '') => {
-               const res = [];
-               for(let i = 0; i < hex.length; i += 2){
-                  res.push(hex.slice(i,i+2));
-               };
-            return res
-               .map(el => String.fromCharCode(parseInt(el, 16)))
-               .join('');
-            };
-            toHex = (ascii = '') => {
-               return ascii
-                  .split('')
-                  .map(el => el.charCodeAt().toString(16))
-                  .join('');
-            };
-         };
+        }  
